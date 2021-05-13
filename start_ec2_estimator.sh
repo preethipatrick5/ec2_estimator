6b6ab07a9c4af3baf9fb80153d272d183db44633
preethipatrick5 <80977546+preethipatrick5@users.noreply.github.com>
2021-05-13 18:06:23 +0100

    sleeping for 60s before starting the estimator

--- start_ec2_estimator.sh
+++ start_ec2_estimator.sh
@@ -5,4 +5,5 @@ sudo apt-get install -y python3-pip
 sudo git clone https://github.com/preethipatrick5/ec2_estimator.git
 cd ec2_estimator
 sudo pip3 install -r requirements.txt
+sleep 60s
 sudo python3 estimator_ec2.py
