e5afe2510e8c4b13f0945fd3852e8fe966d468e7
preethipatrick5 <80977546+preethipatrick5@users.noreply.github.com>
2021-05-13 17:24:12 +0100

    updated startup script

--- start_ec2_estimator.sh
+++ start_ec2_estimator.sh
@@ -1,5 +1,8 @@
 sudo apt-get -y update
+sudo apt-get -y install git
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
+sudo git clone https://github.com/preethipatrick5/ec2_estimator.git
+sudo cd ec2_estimator
 sudo pip3 install -r requirements.txt
 sudo python3 estimator_ec2.py
