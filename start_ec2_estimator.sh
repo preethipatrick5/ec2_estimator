b4e640d4833f03f168d28cccfab582c37e84173d
preethipatrick5 <80977546+preethipatrick5@users.noreply.github.com>
2021-05-13 17:31:07 +0100

    fixed statup script

--- start_ec2_estimator.sh
+++ start_ec2_estimator.sh
@@ -3,6 +3,6 @@ sudo apt-get -y install git
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 sudo git clone https://github.com/preethipatrick5/ec2_estimator.git
-sudo cd ec2_estimator
+cd ec2_estimator
 sudo pip3 install -r requirements.txt
 sudo python3 estimator_ec2.py
